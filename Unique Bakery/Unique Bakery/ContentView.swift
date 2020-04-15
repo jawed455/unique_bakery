9c363fdca807048d091c1b32f9e47de583d699ef
Jawed Ahmad <61515709+jawed455@users.noreply.github.com>
2020-04-14 21:57:46 -0500

    Add files via upload

---
+++ Unique Bakery/Unique Bakery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unique Bakery
+//
+//  Created by Jawed Ahmad on 4/14/20.
+//  Copyright © 2020 Jawed Ahmad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello mero dai kata hunu huncha !")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
